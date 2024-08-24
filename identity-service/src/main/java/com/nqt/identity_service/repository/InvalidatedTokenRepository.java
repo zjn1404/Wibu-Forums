98be15acc090af98bab9abf0ee5e3ec2a9ed7c38
Zjn <129529039+zjn1404@users.noreply.github.com>
2024-08-24 09:43:09 +0700

    Finish AuthenticationService

--- identity-service/src/main/java/com/nqt/identity_service/repository/InvalidatedTokenRepository.java
+++ identity-service/src/main/java/com/nqt/identity_service/repository/InvalidatedTokenRepository.java
@@ -4,4 +4,5 @@ import com.nqt.identity_service.entity.InvalidatedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {
+    boolean existsByRfId(String rfId);
 }
--- identity-service/src/main/java/com/nqt/identity_service/service/authentication/AuthenticationServiceImp.java
+++ identity-service/src/main/java/com/nqt/identity_service/service/authentication/AuthenticationServiceImp.java
@@ -2,10 +2,13 @@ package com.nqt.identity_service.service.authentication;
 
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import com.nqt.identity_service.dto.request.security.*;
 import com.nqt.identity_service.dto.response.AuthenticationResponse;
 import com.nqt.identity_service.dto.response.IntrospectResponse;
+import com.nqt.identity_service.entity.InvalidatedToken;
 import com.nqt.identity_service.entity.User;
 import com.nqt.identity_service.exception.AppException;
 import com.nqt.identity_service.exception.ErrorCode;
@@ -15,17 +18,22 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.text.ParseException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.StringJoiner;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -66,22 +74,78 @@ public class AuthenticationServiceImp implements AuthenticationService {
 
     @Override
     public AuthenticationResponse refreshToken(RefreshTokenRequest request) {
-        return null;
+        try {
+            SignedJWT token = verifyToken(request.getToken(), true);
+
+            String id = token.getJWTClaimsSet().getJWTID();
+            String acId = token.getJWTClaimsSet().getClaim("acId").toString();
+            Date exp = token.getJWTClaimsSet().getExpirationTime();
+
+            invalidatedTokenRepository.save(InvalidatedToken.builder()
+                            .acId(acId)
+                            .rfId(id)
+                            .expiryTime(exp)
+                            .build());
+
+            return buildAuthenticationResponse(userRepository.findByUsername(token.getJWTClaimsSet().getSubject())
+                    .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED)));
+        } catch (Exception e) {
+            throw new AppException(ErrorCode.INVALID_TOKEN);
+        }
     }
 
     @Override
     public IntrospectResponse introspect(IntrospectRequest request) {
-        return null;
+        String token = request.getToken();
+        boolean isValid = true;
+        try {
+            verifyToken(token, false);
+        } catch (Exception e) {
+            isValid = false;
+        }
+
+        return IntrospectResponse.builder()
+                .valid(isValid)
+                .build();
     }
 
     @Override
     public void changePassword(ChangePasswordRequest request) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUsername(authentication.getName())
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
 
+        if (!passwordEncoder.matches(request.getOldPassword(), user.getPassword())) {
+            throw new AppException(ErrorCode.PASSWORD_INCORRECT);
+        }
+
+        if (passwordEncoder.matches(request.getNewPassword(), user.getPassword())) {
+            throw new AppException(ErrorCode.THE_SAME_PASSWORD);
+        }
+
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        userRepository.saveAndFlush(user);
     }
 
     @Override
     public void logout(LogoutRequest request) {
+        try {
+            SignedJWT signedJWT = SignedJWT.parse(request.getToken());
 
+            String acId = signedJWT.getJWTClaimsSet().getJWTID();
+            String rfId = signedJWT.getJWTClaimsSet().getClaim("rfId").toString();
+            Date exp = signedJWT.getJWTClaimsSet().getExpirationTime();
+            exp = new Date(exp.getTime() + (refreshableDuration - validDuration) * 1000);
+
+            invalidatedTokenRepository.save(InvalidatedToken.builder()
+                            .acId(acId)
+                            .rfId(rfId)
+                            .expiryTime(exp)
+                            .build());
+
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+        }
     }
 
     private AuthenticationResponse buildAuthenticationResponse(User user) {
@@ -155,5 +219,30 @@ public class AuthenticationServiceImp implements AuthenticationService {
                 }
         );
         return joiner.toString();
+    }
+
+    private SignedJWT verifyToken(String token, boolean isRefresh) throws JOSEException, ParseException {
+        JWSVerifier verifier = new MACVerifier(accessSignerKey);
+
+        if (isRefresh) {
+            verifier = new MACVerifier(refreshSignerKey);
+        }
+
+        SignedJWT signedJWT = SignedJWT.parse(token);
+
+        Date duration = signedJWT.getJWTClaimsSet().getExpirationTime();
+
+        String id = signedJWT.getJWTClaimsSet().getJWTID();
+        boolean verified = signedJWT.verify(verifier);
+
+        if (!verified || duration.before(new Date())) {
+            throw new AppException(ErrorCode.INVALID_TOKEN);
+        }
+
+        if (invalidatedTokenRepository.existsById(id) || invalidatedTokenRepository.existsByRfId(id)) {
+            throw new AppException(ErrorCode.INVALID_TOKEN);
+        }
+
+        return signedJWT;
     }
 }
